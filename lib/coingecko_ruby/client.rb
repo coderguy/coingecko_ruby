229acb6e57bd0780f5076c44c5f66bb36e4af686
Angel Jose <ajose01@gmail.com>
2021-11-17 21:40:39 -0700

    Enable PRO version

--- lib/coingecko_ruby/client.rb
+++ lib/coingecko_ruby/client.rb
@@ -1,14 +1,14 @@
-require 'coingecko_ruby/connection'
-require 'coingecko_ruby/client/status'
-require 'coingecko_ruby/client/prices'
-require 'coingecko_ruby/client/coins'
-require 'coingecko_ruby/client/categories'
-require 'coingecko_ruby/client/exchanges'
-require 'coingecko_ruby/client/indexes'
-require 'coingecko_ruby/client/finance'
-require 'coingecko_ruby/client/derivatives'
-require 'coingecko_ruby/client/events'
-require 'coingecko_ruby/client/infos'
+require "coingecko_ruby/connection"
+require "coingecko_ruby/client/status"
+require "coingecko_ruby/client/prices"
+require "coingecko_ruby/client/coins"
+require "coingecko_ruby/client/categories"
+require "coingecko_ruby/client/exchanges"
+require "coingecko_ruby/client/indexes"
+require "coingecko_ruby/client/finance"
+require "coingecko_ruby/client/derivatives"
+require "coingecko_ruby/client/events"
+require "coingecko_ruby/client/infos"
 
 module CoingeckoRuby
   class Client
--- lib/coingecko_ruby/connection.rb
+++ lib/coingecko_ruby/connection.rb
@@ -4,7 +4,8 @@ require 'coingecko_ruby/error'
 
 module CoingeckoRuby
   module Connection
-    BASE_URL = 'https://api.coingecko.com/api/v3/'.freeze
+    BASE_URL = "https://api.coingecko.com/api/v3/".freeze
+    BASE_URL_PRO = "https://pro-api.coingecko.com/api/v3".freeze
 
     def get(endpoint, **opts)
       request :get, endpoint, **opts
@@ -20,12 +21,30 @@ module CoingeckoRuby
     end
 
     def create_connection
-      connection = Faraday.new(url: BASE_URL) do |c|
+      url = url() + endpoint + auth()
+
+      connection = Faraday.new(url: url) do |c|
         c.use Faraday::Response::RaiseError
         c.request :retry, max: 5
         c.response :json
       end
       connection
+    end
+
+    def url
+      if @client_version == "pro"
+        BASE_URL_PRO
+      else
+        BASE_URL
+      end
+    end
+
+    def auth
+      if @client_version == "pro"
+        "?x_cg_pro_api_key=" + ENV["COINBASE_PRO_API_KEY"]
+      else
+        ""
+      end
     end
   end
 end
